0a42916e0dbef2188fdd49445d3a1c65b196e8ff
Dan Pegg <daniel.pegg7849@gmail.com>
2021-06-23 23:10:17 +0100

    wrong collection

--- src/Taxonomies/TermRepository.php
+++ src/Taxonomies/TermRepository.php
@@ -4,8 +4,8 @@ namespace Daynnnnn\StatamicDatabase\Taxonomies;
 
 use Statamic\Stache\Repositories\TermRepository as StacheRepository;
 use Statamic\Contracts\Taxonomies\Term as TermContract;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
+use Statamic\Facades\Collection;
 use Statamic\Facades\Site;
 use Statamic\Taxonomies\TermCollection;
 
