5cbd35237146942892b2226fff09115c2dab03d8
Matthew Peck-Deloughry <matt@deloughry.co.uk>
2022-06-25 11:39:33 +0000

    split out configuration further

--- config/statamic-database.php
+++ config/statamic-database.php
@@ -1,14 +1,18 @@
 <?php
 
 return [
-    'assets' => true,
+    'assets_containers' => true,
+    'asset_metas' => true,
     'blueprints' => true,
     'collections' => true,
+    'collection_trees' => true,
     'entries' => true,
     'fieldsets' => true,
     'forms' => true,
     'form_submissions' => true,
     'globals' => true,
     'navigation' => true,
+    'navigation_trees' => true,
     'taxonomies' => true,
+    'terms' => true,
 ];
